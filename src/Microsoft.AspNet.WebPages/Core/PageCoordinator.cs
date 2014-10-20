418c4fa1c9e4758872de6da5807364a340832616
YishaiGalatzer <yigalatz@microsoft.com>
2014-10-19 18:23:19 -0700

    Coordinator is not needed as a base type - removing

--- src/Microsoft.AspNet.WebPages/Core/PageCoordinator.cs
+++ src/Microsoft.AspNet.WebPages/Core/PageCoordinator.cs
@@ -2,19 +2,35 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.Framework.OptionsModel;
 
 namespace Microsoft.AspNet.WebPages.Core
 {
-    public class PageCoordinator : Coordinator
-    {        
+    // This class acts as a controller, but is named differently so it only gets exposed through
+    // the WebPagesActionDescriptorProvider.
+    public class PageCoordinator
+    {
+        [Activate]
+        public ActionContext Context { get; set; }
+
+        [Activate]
+        public IOptions<WebPagesOptions> Options { get; set; }
+
+        [Activate]
+        public ICompositeViewEngine ViewEngine { get; set; }
+
+        [Activate]
+        public ViewDataDictionary ViewData { get; set; }
+
         [WebPagesDefaultActionConvention]
         public IActionResult WebPagesView(string __viewPath)
         {
-            var viewPath = Options.Options.PagesFolderPath.TrimEnd(PathSeparators)
+            var viewPath = Options.Options.PagesFolderPath.TrimEnd(Coordinator.PathSeparators)
                 + "/"
                 + __viewPath;
 
-            var result = ViewEngine.FindView(ActionContext, viewPath);
+            var result = ViewEngine.FindView(Context, viewPath);
 
             if (result.Success)
             {
--- src/Microsoft.AspNet.WebPages/Core/RoutedPageCoordinator.cs
+++ src/Microsoft.AspNet.WebPages/Core/RoutedPageCoordinator.cs
@@ -2,17 +2,27 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
 
 namespace Microsoft.AspNet.WebPages.Core
 {
     // This class acts as a controller, but is named differently so it only gets exposed through
     // the WebPagesActionDescriptorProvider.
-    public class RoutedPageCoordinator : Coordinator
+    public class RoutedPageCoordinator
     {
+        [Activate]
+        public ActionContext Context { get; set; }
+
+        [Activate]
+        public ICompositeViewEngine ViewEngine { get; set; }
+
+        [Activate]
+        public ViewDataDictionary ViewData { get; set; }
+
         [WebPagesDefaultActionConvention]
         public IActionResult WebPagesView(string __viewPath)
         {
-            var result = ViewEngine.FindView(ActionContext, __viewPath);
+            var result = ViewEngine.FindView(Context, __viewPath);
 
             if (result.Success)
             {
