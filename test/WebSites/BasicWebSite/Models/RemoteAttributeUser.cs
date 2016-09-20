5465a30020cb38d9b71fa5fd19d26e07e7a298ba
Ryan Brandenburg <rybrande@microsoft.com>
2016-09-20 16:49:27 -0700

    Example validation

--- test/WebSites/BasicWebSite/Models/RemoteAttributeUser.cs
+++ test/WebSites/BasicWebSite/Models/RemoteAttributeUser.cs
@@ -12,32 +12,20 @@ namespace BasicWebSite.Models
 
         // Controller in current area.
         [Required(ErrorMessage = "UserId1 is required")]
-        [Remote(action: "IsIdAvailable", controller: "RemoteAttribute_Verify")]
         public string UserId1 { get; set; }
 
         // Controller in root area.
         [Required(ErrorMessage = "UserId2 is required")]
-        [Remote(action: "IsIdAvailable", controller: "RemoteAttribute_Verify", areaName: null, HttpMethod = "Post")]
         public string UserId2 { get; set; }
 
+        [MinLength(6)]
         [Required(ErrorMessage = "UserId3 is required")]
-        [Remote(
-            action: "IsIdAvailable",
-            controller: "RemoteAttribute_Verify",
-            areaName: "Area1",
-            ErrorMessage = "/Area1/RemoteAttribute_Verify/IsIdAvailable rejects you.")]
         public string UserId3 { get; set; }
 
         [Required(ErrorMessage = "UserId4 is required")]
-        [Remote(
-            action:"IsIdAvailable",
-            controller: "RemoteAttribute_Verify",
-            areaName: "Area2",
-            AdditionalFields = "UserId1, UserId2, UserId3")]
         public string UserId4 { get; set; }
 
         [Required(ErrorMessage = "UserId5 is required")]
-        [Remote(routeName: "VerifyRoute", HttpMethod = "Post")]
         public string UserId5 { get; set; }
     }
 }
